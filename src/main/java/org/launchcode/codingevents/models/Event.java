bae5187a40c9f0ab0db3ee1819f03aa3cd6deab9
Chris Bay <chris@chrisbay.net>
2022-09-14 14:50:01 -0500

    Implements AbstractEntity

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,26 +1,14 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.Objects;
 
 @Entity
-public class Event {
-
-    // configure id to be the PK for this class
-    @Id
-    @GeneratedValue
-    private int id;
+public class Event extends AbstractEntity {
 
     private EventType type;
-
-    @NotBlank(message="Name is required")
-    @Size(min = 3, max = 50, message="Name must be between 3 and 50 characters")
-    private String name;
 
     @Size(max = 500, message="Description too long (maximum length is 500)")
     private String description;
@@ -30,22 +18,14 @@ public class Event {
     private String contactEmail;
 
     public Event(String name, String description, String contactEmail, EventType type) {
-        this.name = name;
+        super(name);
         this.description = description;
         this.contactEmail = contactEmail;
         this.type = type;
     }
 
     // Hibernate needs no-arg constructors
-    public Event() { }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public Event() {}
 
     public String getDescription() {
         return description;
@@ -53,10 +33,6 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getContactEmail() {
@@ -73,18 +49,5 @@ public class Event {
 
     public void setType(EventType type) {
         this.type = type;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
